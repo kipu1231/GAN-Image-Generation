ca36f34cfa15a042cc94059f774daafe2cbda7b2
Chiara Pullem <chiara.pullem@gmx.de>
2020-03-25 09:54:56 +0100

    updated link to data

--- get_data.sh
+++ get_data.sh
@@ -1,8 +1,11 @@
 # Download dataset from Dropbox
-wget https://www.dropbox.com/preview/Modelle%20Deep%20Learning/GAN/face.zip
+wget https://www.dropbox.com/s/r2xh8p3kqtc9h1t/face.zip
 
 # Unzip the downloaded zip file
 unzip ./face.zip 
 
 # Remove the downloaded zip file
 rm ./face.zip
+
+# Remove the MACOSX folder
+rm -rf ./__MACOSX/
